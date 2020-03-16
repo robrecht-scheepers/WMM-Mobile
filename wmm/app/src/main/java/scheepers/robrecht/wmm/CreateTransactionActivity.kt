af7efcae1e1106c82643606a7f9732f9579ed894
scheepers <robrecht.scheepers@gmail:co>
2020-03-16 16:29:09 +0100

    Improved date handling

--- wmm/app/src/main/java/scheepers/robrecht/wmm/CreateTransactionActivity.kt
+++ wmm/app/src/main/java/scheepers/robrecht/wmm/CreateTransactionActivity.kt
@@ -10,46 +10,52 @@ import kotlinx.android.synthetic.main.activity_create_transaction.*
 import java.util.*
 
 class CreateTransactionActivity : AppCompatActivity() {
-    private val amountText by lazy { findViewById<EditText>(R.id.amountText) };
+    private var date:Calendar = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_transaction)
 
+        // numeric keypad button handlers
         val numberButtonListener = View.OnClickListener { v ->
             val btn = v as Button
             amountText.append(btn.text)
         }
-
-        findViewById<Button>(R.id.button0).setOnClickListener(numberButtonListener)
-        findViewById<Button>(R.id.button1).setOnClickListener(numberButtonListener)
-        findViewById<Button>(R.id.button2).setOnClickListener(numberButtonListener)
-        findViewById<Button>(R.id.button3).setOnClickListener(numberButtonListener)
-        findViewById<Button>(R.id.button4).setOnClickListener(numberButtonListener)
-        findViewById<Button>(R.id.button5).setOnClickListener(numberButtonListener)
-        findViewById<Button>(R.id.button6).setOnClickListener(numberButtonListener)
-        findViewById<Button>(R.id.button7).setOnClickListener(numberButtonListener)
-        findViewById<Button>(R.id.button8).setOnClickListener(numberButtonListener)
-        findViewById<Button>(R.id.button9).setOnClickListener(numberButtonListener)
-        findViewById<Button>(R.id.buttonComma).setOnClickListener(numberButtonListener)
-
-        findViewById<Button>(R.id.buttonErase).setOnClickListener { v ->
+        button0.setOnClickListener(numberButtonListener)
+        button1.setOnClickListener(numberButtonListener)
+        button2.setOnClickListener(numberButtonListener)
+        button3.setOnClickListener(numberButtonListener)
+        button4.setOnClickListener(numberButtonListener)
+        button5.setOnClickListener(numberButtonListener)
+        button6.setOnClickListener(numberButtonListener)
+        button7.setOnClickListener(numberButtonListener)
+        button8.setOnClickListener(numberButtonListener)
+        button9.setOnClickListener(numberButtonListener)
+        buttonComma.setOnClickListener(numberButtonListener)
+        buttonErase.setOnClickListener { v ->
             if(amountText.text.isNotEmpty())
                 amountText.setText(amountText.text.substring(0,amountText.text.length - 1))
         }
 
-        findViewById<Button>(R.id.buttonDate).setOnClickListener { v ->
-            val c = Calendar.getInstance()
-            val year = c.get(Calendar.YEAR)
-            val month = c.get(Calendar.MONTH)
-            val day = c.get(Calendar.DAY_OF_MONTH)
-
-            val datePicker = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{v, y, m, d ->
-                dateText.setText("$d.$m.$y")}, year, month, day)
+        // date
+        updateDateText()
+        buttonDate.setOnClickListener { v ->
+            val datePicker = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{
+                    v, y, m, d ->
+                run {
+                    updateDateText(d, m, y)
+                    date.set(y, m, d)
+                }
+            },
+                date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH))
             datePicker.show()
         }
 
+        
+    }
 
-
+    private fun updateDateText()
+    {
+        dateText.setText("${date.get(Calendar.YEAR)};${date.get(Calendar.MONTH)}.${date.get(Calendar.DAY_OF_MONTH)}")
     }
 }
