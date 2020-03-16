3529a7474d3869e87a84414191226fec7aa6cb72
scheepers <robrecht.scheepers@gmail:co>
2020-03-16 21:01:26 +0100

    State saving and next activity

--- wmm/app/src/main/java/scheepers/robrecht/wmm/CreateTransactionActivity.kt
+++ wmm/app/src/main/java/scheepers/robrecht/wmm/CreateTransactionActivity.kt
@@ -1,16 +1,25 @@
 package scheepers.robrecht.wmm
 
 import android.app.DatePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_create_transaction.*
+import java.lang.NumberFormatException
 import java.util.*
+
+private const val LOGTAG: String = "CreateTransaction"
+private const val AMOUNT: String = "amount"
+private const val DATE: String = "date"
 
 class CreateTransactionActivity : AppCompatActivity() {
     private var date:Calendar = Calendar.getInstance()
+    private var amount:Double? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +29,7 @@ class CreateTransactionActivity : AppCompatActivity() {
         val numberButtonListener = View.OnClickListener { v ->
             val btn = v as Button
             amountText.append(btn.text)
+            updateAmount()
         }
         button0.setOnClickListener(numberButtonListener)
         button1.setOnClickListener(numberButtonListener)
@@ -33,8 +43,10 @@ class CreateTransactionActivity : AppCompatActivity() {
         button9.setOnClickListener(numberButtonListener)
         buttonComma.setOnClickListener(numberButtonListener)
         buttonErase.setOnClickListener { v ->
-            if(amountText.text.isNotEmpty())
-                amountText.setText(amountText.text.substring(0,amountText.text.length - 1))
+            if(amountText.text.isNotEmpty()) {
+                amountText.setText(amountText.text.substring(0, amountText.text.length - 1))
+                updateAmount()
+            }
         }
 
         // date
@@ -43,19 +55,57 @@ class CreateTransactionActivity : AppCompatActivity() {
             val datePicker = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{
                     v, y, m, d ->
                 run {
-                    updateDateText(d, m, y)
                     date.set(y, m, d)
+                    updateDateText()
                 }
-            },
-                date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH))
+            },date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH))
             datePicker.show()
         }
 
-        
+        // continue
+        buttonContinue.setOnClickListener { v ->
+            val intent = Intent(this, FinishTransactionActivity::class.java)
+            if(amount != null) {
+                intent.putExtra("amount", amount)
+            }
+            intent.putExtra("date", date)
+            startActivity(intent)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(LOGTAG, "onSaveInstanceState")
+        super.onSaveInstanceState(outState)
+        if(amount !=null) {
+            outState.putDouble(AMOUNT, amount!!)
+        }
+        outState.putSerializable(DATE, date)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        if(savedInstanceState.containsKey(AMOUNT)) {
+            amount = savedInstanceState.getDouble(AMOUNT)
+        }
+        date = savedInstanceState.getSerializable(DATE) as Calendar
     }
 
     private fun updateDateText()
     {
-        dateText.setText("${date.get(Calendar.YEAR)};${date.get(Calendar.MONTH)}.${date.get(Calendar.DAY_OF_MONTH)}")
+        dateText.setText(
+            "${date.get(Calendar.YEAR)};${date.get(Calendar.MONTH)}.${date.get(Calendar.DAY_OF_MONTH)}")
+    }
+
+    private fun updateAmount()
+    {
+        amount = try {
+            if(amountText.text.isNotEmpty())
+                amountText.text.toString().toDouble()
+            else
+                null
+        } catch(e:NumberFormatException) {
+            null
+        }
     }
 }
---
+++ wmm/app/src/main/java/scheepers/robrecht/wmm/FinishTransactionActivity.kt
@@ -0,0 +1,12 @@
+package scheepers.robrecht.wmm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FinishTransactionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finish_transaction)
+    }
+}
