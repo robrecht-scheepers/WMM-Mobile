a1c4e9dfcd84f9fe71d36ee337df1b0b24cfeb48
scheepers <robrecht.scheepers@gmail:co>
2020-03-17 13:48:13 +0100

    spinner handling

--- wmm/app/src/main/java/scheepers/robrecht/wmm/FinishTransactionActivity.kt
+++ wmm/app/src/main/java/scheepers/robrecht/wmm/FinishTransactionActivity.kt
@@ -2,6 +2,8 @@ package scheepers.robrecht.wmm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_finish_transaction.*
 import java.util.*
@@ -11,20 +13,39 @@ private const val AMOUNT: String = "amount"
 private const val DATE: String = "date"
 
 class FinishTransactionActivity : AppCompatActivity() {
+    private lateinit var date:Calendar
+    private var amount: Double = 0.0
+    private var category: String = ""
+    private var comments: String = ""
+
+    private val categories = arrayOf("Einkaufen", "Kinderkleidung", "Restaurant")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish_transaction)
 
-        val date: Calendar = intent.getSerializableExtra(DATE) as Calendar
+        // init date and amount passed from previous activity
+        date = intent.getSerializableExtra(DATE) as Calendar
         dateText.text =
             "${date.get(Calendar.DAY_OF_MONTH)}.${date.get(Calendar.MONTH)}.${date.get(Calendar.YEAR)}"
+        amount = intent.getDoubleExtra(AMOUNT, 0.0)
+        amountText.text = amount.toString()
 
-        amountText.text = intent.getDoubleExtra(AMOUNT, 0.0).toString()
-
-        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item,
-            listOf("Einkaufen", "Kinderkleidung", "Restaurant"))
+        // init category spinner
+        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         categorySpinner.adapter = spinnerAdapter
+
+        categorySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?,position: Int,id: Long){
+                category = categories[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                category = ""
+            }
+        }
+
+        
     }
 }
